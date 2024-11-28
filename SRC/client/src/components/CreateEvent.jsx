53d488bd5edd387e1d12daf8f4341d7c8b63beb9
OmeetK <oomeetka@uwo.ca>
2024-11-28 18:08:06 -0500

    event api updated

---
+++ SRC/client/src/components/CreateEvent.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import UserHeader from "./UserHeader";
+import axios from "axios";
+import backgroundImage from "../img/back.png";
+
+function CreateEvent() {
+    return (
+        <div style={styles.page}>
+            <h2>Login Page</h2>
+            {/* Add your login form or content here */}
+        </div>
+    );
+}
+
+export default CreateEvent;
--- SRC/server/server.js
+++ SRC/server/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Shadowflash1",
+    password: "Idunknow2!",
     database: "BRATmusic",
 });
 
