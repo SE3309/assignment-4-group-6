6ef3cf39bc73f5073a03ecdff09ef3063cf5db17
OmeetK <oomeetka@uwo.ca>
2024-11-28 21:30:34 -0500

    event pages created and updated

--- SRC/client/src/App.jsx
+++ SRC/client/src/App.jsx
@@ -7,6 +7,8 @@ import ArtistWelcomePage from './components/ArtistWelcomePage'
 import SearchArtist from './components/SearchArtist'
 import SearchSong from "./components/SearchSong";
 import DisplayEvent from "./components/DisplayEvent";
+import CreateEvent from "./components/CreateEvent";
+
 
 function App() {
   return (
@@ -18,7 +20,9 @@ function App() {
         <Route path="/Artist" element={<ArtistWelcomePage />} />
         <Route path="/SearchArtist" element={<SearchArtist />} />
         <Route path="/SearchSong" element={<SearchSong />} />
-        <Route path="/DisplayEvent" element={<DisplayEvent />} />        
+        <Route path="/DisplayEvent" element={<DisplayEvent />} /> 
+        <Route path="/CreateEvent" element={<CreateEvent />} />        
+       
 
       </Routes>
     </BrowserRouter>
