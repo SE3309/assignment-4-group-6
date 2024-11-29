a737e2bad4ade08244bfefc3848f95b294991f4c
OmeetK <oomeetka@uwo.ca>
2024-11-28 23:39:43 -0500

    Add artist to nav bar

--- SRC/client/src/components/UserHeader.jsx
+++ SRC/client/src/components/UserHeader.jsx
@@ -61,6 +61,7 @@ const UserHeader = () =>
                   <Link to="/login" style={linkStyle}>Revenue Generated</Link>
                   <Link to="/SearchAlbum" style={linkStyle}>Album</Link>
                   <Link to="/SearchSong" style={linkStyle}>Song</Link>
+                  <Link to="/SearchArtist" style={linkStyle}>Artist</Link>
                   <Link to="/DisplayEvent" style={linkStyle}>Event</Link>
                   <Link to="/ProfileSettings" style={linkStyle}>Settings</Link>                 
                   <Link to="/" style={linkStyle}>Log out</Link>
