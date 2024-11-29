2aa6a376e639ccaf742df1dc286f0fd044aa3d99
stheri0330 <stheri@uwo.ca>
2024-11-28 22:47:29 -0500

    updates for simone

--- SRC/client/src/App.jsx
+++ SRC/client/src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import WelcomePage from "./components/WelcomePage";
 import UserWelcomePage from "./components/UserWelcomePage";
 import UserHeader from "./components/UserHeader";
 import ArtistWelcomePage from './components/ArtistWelcomePage';
